34bf43f29c2c5fcd56f0a33f9933ca1174bdc7c4
Omar Dahmash <omartdh@Omars-MacBook-Pro.local>
2021-06-03 14:19:43 -0700

    modified footer and overall layout

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,21 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <span>© { new Date().getFullYear() } Omar Dahmash </span>
-    </footer>
+
+    <footer>
+    <div className="footer-content">
+        <p>Designed and developed by:</p>
+        <h3>Omar Dahmash</h3>
+        <ul className="socials">
+            <li><a href="https://www.instagram.com/omtdh/"><i className="bx bxl-instagram"></i></a>
+            <a href="https://github.com/omartdh"><i className="bx bxl-github"></i></a>
+            <a href="https://www.linkedin.com/in/omar-dahmash-b34693a4/"><i className="bx bxl-linkedin"></i></a></li>
+        </ul>
+    </div>
+    <div className="footer-bottom">
+        <p>copyright &copy;{ new Date().getFullYear() } Omar Dahmash</p>
+    </div>
+</footer>
   );
 }
 
