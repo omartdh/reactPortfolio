d133f22f4140eb3440a392428296fe4cddf8b62e
Omar Dahmash <omartdh@omars-mbp.home>
2021-06-05 00:28:33 -0700

    modified css everywhere

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,9 +9,11 @@ function Footer() {
         <p>Designed and developed by:</p>
         <h3>Omar Dahmash</h3>
         <ul className="socials">
-            <li><a href="https://www.instagram.com/omtdh/"><i className="bx bxl-instagram"></i></a>
+            <li>
             <a href="https://github.com/omartdh"><i className="bx bxl-github"></i></a>
-            <a href="https://www.linkedin.com/in/omar-dahmash-b34693a4/"><i className="bx bxl-linkedin"></i></a></li>
+            <a href="https://www.linkedin.com/in/omar-dahmash-b34693a4/"><i className="bx bxl-linkedin"></i></a>
+            <a href="https://www.instagram.com/omtdh/"><i className="bx bxl-instagram"></i></a>
+            </li>
         </ul>
     </div>
     <div className="footer-bottom">
